2cf3f7a65dd259765cd5979f0850ef45996b32cc
Nathaniel Factor <bbnat123@outlook.com>
2024-01-31 17:59:39 -0500

    created helloworld init com

---
+++ helloworld.py
@@ -0,0 +1,3 @@
+print("hello world")
+# hi everyone this is our git hub for our coding for all of our projects! I will setup the branches later
+# but for now just make sure you guys get setup as a collaborator!
