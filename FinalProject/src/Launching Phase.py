60f16a1e715b3676ccf575578ba748765882a4e9
J-Di <xjames99x@gmail.com>
2024-04-04 20:22:29 -0400

    added tolerences

--- FinalProject/src/Launching Phase.py
+++ FinalProject/src/Launching Phase.py
@@ -11,8 +11,7 @@ from types import FunctionType
 BP = brickpi3.BrickPi3() # Initialize the brickPi
 
 # Colour port connection
-COLOR_SENSOR = EV3ColorSensor() 
-
+COLOR_SENSOR = EV3ColorSensor(3) 
 # Motor Connections and initialization
 LAUNCH_MOTOR = BP.PORT_C
 MOTOR_INTAKE = BP.PORT_D
@@ -62,19 +61,23 @@ def is_red():
 
         #COLOR_SENSOR
         color_lvl = COLOR_SENSOR.get_rgb()
+        print("Red value read is:", color_lvl)
+        red_lvl, green_lvl, blue_lvl = color_lvl[0], color_lvl[1], color_lvl[2]
 
-        if color_lvl[0] > 30: # Red is the first value in the color array
+        # Check to see if red is within color ranges experimentally tested 
+        if (red_lvl > 175 and red_lvl < 185) and (green_lvl > 44 and green_lvl < 52) and ( blue_lvl > 12 and blue_lvl < 20): 
             print("red detected")
             return True
         else:
             return False 
 
 
-def main ():
+def main (RED_DETECTED):
     while True:
         sleep(0.2)
         if RED_DETECTED == 0:
             if (is_red()):
+                print("hit")
                 RED_DETECTED += 1
                 sleep(30) # wait 30 seconds for the TA to load the robot, and for the robot to leave red region
 
@@ -91,6 +94,4 @@ if __name__ == "__main__":
 
     sleep(2)
     print("Robot initializing...")
-    # colourDetection()
-
-launch ()
+    main(RED_DETECTED)
